94cc2d4025bc920e7befda10ff1638cb3e9a4051
Shubendu Biswas <biswasshubendu4@gmail.com>
2023-02-22 23:09:05 +0530

    done with store basic apis

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 stores = [{"name": "My Store", "items": [{"name": "my item", "price": 15.99}]}]
 
 @app.get("/store")
-def get_store():
+def get_stores():
     return {"stores": stores}
 
 
@@ -16,3 +16,33 @@ def create_store():
     new_store = {"name": request_data["name"],"items":[]}
     stores.append(new_store)
     return new_store, 201
+
+#dynamically adding  item in store from url
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+
+
+
+@app.get("/store/<string:name>/items")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
+
+    
