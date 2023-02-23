f774176683f3476f926dd3495a41a750cf75a989
shubendu <biswasshubendu4@gmail.com>
2023-02-23 17:20:54 +0530

    added few more apis

--- app.py
+++ app.py
@@ -1,48 +1,113 @@
 from flask import Flask, request
+from db import stores,items
+from flask_smorest import abort
+import uuid
 
 app = Flask(__name__)
 
 
-stores = [{"name": "My Store", "items": [{"name": "my item", "price": 15.99}]}]
-
+#all stores data
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
 
-
+#create store
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"],"items":[]}
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="bad request, ensure name is included in the json payload")
+    print("="*50)
+    print(stores.values())
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message="store already exists")
+    store_id =  uuid.uuid4().hex
+    store = {**store_data, "id":store_id}
+    stores[store_id] = store 
+    return store, 201
 
-#dynamically adding  item in store from url
-@app.post("/store/<string:name>/item")
-def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item
-    return {"message": "Store not found"}, 404
+#create item
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    #error handling
+    #here not only we need to validate data exitst,
+    #but also what type of data. for example price should be in float
+    if ("price" not in item_data or "store_id" not in item_data or "name" not in item_data):
+        abort(400, message="Bad request. Ensure price storeid and name are  included in the  JSON payload")
+        
+    for item in items.values():
+        if(
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store-id"]
+        ):
+            abort(400,message="item already exists")
+            
+    if item_data["store_id"] not in stores:
+        abort(404, message="store not found")
+    
+    item_id =  uuid.uuid4().hex
+    item = {**item_data, "id":item_id}
+    items[item_id] = item 
+    return item, 201
+
+#get all items information
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+#get individual store
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:   
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="store not found")
+        
+#updating item
+@app.put("/item/<string:item_id>")
+def upddate_item(item_id):
+    item_data  = request.get_json()
+
+    if ("price" not in item_data or "name" not in item_data):
+        abort(400, message="Bad request. Ensure price and name are  included in the  JSON payload")
+    
+    try:
+        item = items[item_id]
+        
+        #inpace update of dictionary
+        item |= item_data
+        return item
+    except KeyError:
+        abort(404, message="Item not found")
 
 
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    return {"message": "Store not found"}, 404
+#get individual item
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:   
+        return items[item_id]
+    except KeyError:
+        abort(404, message="item not found")
+        
 
-
-
-@app.get("/store/<string:name>/items")
-def get_item_in_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"items": store["items"]}
-    return {"message": "Store not found"}, 404
+#delete an item
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message":"Item deleted"}
+    except KeyError:
+        abort(404, message="Item not found")
+        
+#delete store
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message":"store deleted"}
+    except KeyError:
+        abort(404, message="store not found")
 
     
